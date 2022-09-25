27589df62c39604ed2794e775efc9e0dc5096c0c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-09-25 04:58:04 -0400

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,16 @@
+using Documenter
 
+makedocs(
+    modules = [Double64s],
+    sitename = "Double64s.jl",
+    authors = "Jeffrey Sarnoff and other contributors",
+    pages  = Any[
+        "Overview"                 => "index.md",
+        "The Types"                => "types.md",
+        ]
+    )
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/Double64s.jl.git",
+    target = "build"
+)
