bb5c8304200b307d04913e4843263226c186b61c
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-09-25 05:43:11 -0400

    Update Double64.jl

--- src/type/Double64.jl
+++ src/type/Double64.jl
@@ -1,1 +1,24 @@
+struct FloatD64 <: AbstractFloat
+    hi::Float64
+    lo::Float64
+  
+    FloatD64(hi::Float64, lo::Float64) =
+        new(hi, lo)
+end
+
+function Double64(a::Float64, b::Float64)
+    hi, lo = two_sum(a, b)
+    FloatD64(hi, lo)
+end
+  
+hi(x::FloatD64) = x.hi
+lo(x::FloatD64) = x.lo
+
+hilo(x::FloatD64) = (x.hi, x.lo)
+lohi(x::FloatD64) = (x.lo, x.hi)
+
+function Base.show(io::IO, ::MIME"text/plain", x::FloatD64)
+      str = string("Double64(", x.hi, ", ", x.lo, ")")
+      print(io, str)
+end
 
