425c3d7a53d0b306c728da3909c11f1c93e0a5c8
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-09-25 23:37:02 -0400

    HiLoSchema

--- src/support/twotuple.jl
+++ src/support/twotuple.jl
@@ -7,13 +7,13 @@ lo(ab::NTuple{2,T}) where {T} = ab[2]
 twotuple(a, b) = (a, b)
 twotuple(ab::NTuple{2,T}) where {T} = ab
 
-const HILO = NamedTuple{(:hi, :lo), Tuple{Float64, Float64})
+const HiLoSchema = NamedTuple{(:hi, :lo), Tuple{Float64, Float64}}
 
 hi(x::HILO) = x.hi
 lo(x::HILO) = x.lo
 
-hilo(a, b) = HILO((a, b))
-hilo(ab::NTuple{2,Float64}) = HILO(ab)
+hilo(a, b) = HiLoSchema((a, b))
+hilo(ab::NTuple{2,Float64}) = HiLoSchema(ab)
 
 hilotuple(a, b) = hilo(max_min_mag(a,b))
 hilotuple(ab::NTuple{2,T}) where {T} = hilo(max_min_mag(ab[1], ab[2]))
