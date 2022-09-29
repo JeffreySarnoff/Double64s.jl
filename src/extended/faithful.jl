94db12ad80769fbc0f74250d562a123f50319070
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-09-29 08:25:39 -0400

    varnames

--- src/extended/faithful.jl
+++ src/extended/faithful.jl
@@ -36,84 +36,83 @@ end
 hi(x::CPair) = x.hi
 lo(x::CPair) = x.lo
 hilo(x::CPair) = (x.hi, x.lo)
+function CPairSum(ahi::T, alo::T, bhi::T, blo::T) where {T}
+    c = ahi + bhi
+    t = ahi + bhi - c
+    s = alo + blo
+    g = t + s
+    (c, g)
+end
 
-function CPairSum(ae::CPair{T}, bf::CPair{T}) where {T}
-    a, e = hilo(ae)
-    b, f = hilo(bf)
-    c = a + b
-    t = a + b - c
-    s = e + f
+function CPairSum(a::CPair{T}, b::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    bhi, blo = hilo(bf)
+    CPairSum(ahi, alo, bhi, blo)
+end
+
+function CPairSum(a::CPair{T}, b::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    bhi, blo = hilo(bf)
+    c = ahi + bhi
+    t = ahi + bhi - c
+    s = alo + blo
     g = t + s
     CPair(c, g)
 end
 
-function CPairDiff(ae::CPair{T}, bf::CPair{T}) where {T}
-    a, e = hilo(ae)
-    b, f = hilo(bf)
-    c = a - b
-    t = a - b - c
-    s = e - f
+function CPairDiff(a::CPair{T}, b::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    bhi, blo = hilo(bf)
+    c = ahi - bhi
+    t = ahi - bhi - c
+    s = alo - blo
     g = t + s
     CPair(c, g)
 end
 
-function CPairProd(ae::CPair{T}, bf::CPair{T}) where {T}
-    a, e = hilo(ae)
-    b, f = hilo(bf)
-    c = a * b
+function CPairProd(a::CPair{T}, b::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    bhi, blo = hilo(bf)
+    c = ahi * bhi
     t = fma(a, b, -c)
-    q = a * f
-    r = b * e
+    q = ahi * blo
+    r = bhi * alo
     s = q + r
     g = t + s
     CPair(c, g)
 end
 
-function CPairDiv(ae::CPair{T}, bf::CPair{T}) where {T}
-    a, e = hilo(ae)
-    b, f = hilo(bf)
-    c = a / b
-    t = fma(-b, c, a)
-    p = t + e
-    q = c * f
+function CPairDiv(a::CPair{T}, b::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    bhi, blo = hilo(bf)
+    c = ahi / bhi
+    t = fma(-bhi, c, ahi)
+    p = t + alo
+    q = c * blo
     r = p - q
-    s = b + f
+    s = bhi + blo
     g = r / s
     CPair(c, g)
 end
 
-function CPairInv(bf::CPair{T}) where {T}
-    b, f = hilo(bf)
-    c = one(T) / b
-    t = fma(-b, c, one(T))
-    q = c * f
+function CPairInv(b::CPair{T}) where {T}
+    bhi, blo = hilo(bf)
+    c = one(T) / bhi
+    t = fma(-bhi, c, one(T))
+    q = c * blo
     r = t - q
-    s = b + f
+    s = bhi + blo
     g = r / s
     CPair(c, g)
 end
 
-function CPairSqrt(ae::CPair{T}) where {T}
-    a, e = hilo(ae)
-    c = sqrt(a)
-    t = fma(-c, c, a)
-    r = t + e
+function CPairSqrt(a::CPair{T}) where {T}
+    ahi, alo = hilo(ae)
+    c = sqrt(ahi)
+    t = fma(-c, c, ahi)
+    r = t + alo
     s = c + c
     g = r / s
     CPair(c, g)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
